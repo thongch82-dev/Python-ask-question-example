ab3db92e2e2cd8bc54d893b5c727f23105becce5
thongch82-dev <thongch82@gmail.com>
2025-10-12 13:50:43 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,1 @@
 print("Hello! ")
-answer = input("How are you? ")
-good = {"good","g","well","verywell","verygood","happy"}
-bad = {"bad","b","sad","verybad","verysad","unwell","veryunwell"}
-if answer.strip().lower() in good:
-  print("good 👍 ")
-elif answer.strip().lower() in bad:
-  print("really? ")
-else:
-  print("what are you saying? ")
